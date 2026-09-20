6eec22627a2e15b7627fd482d57b565405850d8f
ShFsn <tigr.tigrov.1977@gmail.com>
2026-09-20 22:23:54 +0300

    1.3.6 Макросы логирования

--- 134-led-module/main.c
+++ 134-led-module/main.c
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "led.h"
+#include "log.h"
 
 
 
@@ -18,16 +19,20 @@ bool get_button_debounce(uint pin) {
 
 
 void handle_command(int command) {
+	LOG_DBG("got %c\n", command);
 	if (command == 'e') {
 		led_set(true);
-		printf("led %s\n", led_is_on() ? "on" : "off");
+		LOG_INF("led %s\n", led_is_on() ? "on" : "off");
 	}
 	else if (command == 'd') {
 		led_set(false);
-		printf("led %s\n", led_is_on() ? "on" : "off");
+		LOG_INF("led %s\n", led_is_on() ? "on" : "off");
+	}
+	else if (command == 'v') {
+		log_version();
 	}
 	else {
-		printf("unknown command: %c\n", command);
+		LOG_ERR("unknown command: %c\n", command);
 	}
 
 	return;
